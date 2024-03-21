e77f687699f74e3de9deca034907203287fbf254
Ari. <69asdfghjkl00@gmail.com>
2024-03-21 14:12:05 +0700

    add: settings

--- _example/main.go
+++ _example/main.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
-	raw := tcd.New().FetchData()
+	tcd.UserAgent = "your user-agent. WHERE?? put here!"
+	q := tcd.New()
+	q.PageSize = "3"
+
+	raw := q.FetchData()
 	raw.GetFileBulk()
 }
